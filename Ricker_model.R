aaf0040fac5d8da8c9b0c754565bd8c0af053ccd
Christie Bahlai <cbahlai@msu.edu>
2018-08-02 10:39:48 -0400

    add timeseries fiddler toy code

--- Ricker_model.R
+++ Ricker_model.R
@@ -7,13 +7,13 @@
 #our N(t) vector-ie the population of an organism on day t
 
 #set maximum population
-maxpop<-50
+maxpop<-3000
 Nt<-(1:maxpop)
 
 #set carrying capacity
-k<-30
+k<-2000
 #set intrinsic rate of increase
-r<-4
+r<-2.3
 
 #Ricker model predicts population at next time step Nt1 using Nt, r and k
 Nt1<- Nt*exp(r*(1- Nt/k))
@@ -51,3 +51,29 @@ gg<-ggplot(simulation, aes(x=Nt, y=value, color=variable))+
   scale_x_continuous(name="N(t)")+
   scale_y_continuous(name="N(t+1)")
 gg
+
+
+#timeseries
+
+steps<-25
+years<-(1:25)
+Num<-c()
+Nt1<-maxpop
+
+for (i in 1:steps){
+  Num<-c(Num, Nt1)
+  Nt1<- Nt1*exp(r*(1- Nt1/k))
+  
+}
+
+timeseries<-as.data.frame(cbind(years, Num))
+
+
+gg2<-ggplot(timeseries, aes(x=years, y=Num))+
+  geom_point()+
+  geom_line()+
+  scale_x_continuous(name="Year")+
+  scale_y_continuous(name="N(t)")
+gg2
+
+
